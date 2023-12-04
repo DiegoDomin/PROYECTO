854b1c091aa532fa847f71e48dea3a5603dc70fd
Alexandra <00164722@uca.edu.sv>
2023-12-04 00:52:52 -0600

    Video responsive

--- proyecto-equipo-10-sec-01-div-squadron-develop/Client/src/components/quienes-somos/quienes-somos.jsx
+++ proyecto-equipo-10-sec-01-div-squadron-develop/Client/src/components/quienes-somos/quienes-somos.jsx
@@ -8,29 +8,30 @@ const QuienesSomos = () => {
                 <h1>¿QUIENES SOMOS?</h1>
             </div>
             <div className="tit">
-            <h2> MIRAME</h2>
+                <h2> MIRAME</h2>
             </div>
-            
-            <div className="video">
-                
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/6aPfrs6J490?si=gb2Ft_d9w58C-avZ" title="YouTube video player" 
-                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
 
-                </iframe>
+            <div className="video">
+                <iframe 
+                    style={{ width: "70%", height: "70%" }}
+                    src="https://www.youtube.com/embed/6aPfrs6J490?si=gb2Ft_d9w58C-avZ"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                />
             </div>
             <img src={pilarestitulo} alt="pilares titulo" className="banner" />
             <p className="texto">
                 Somos una asociación que busca tener un monitoreo de todas las zonas del
                 país con el fin de lograr informar a la población el estado actual de la
-                región, esto gracias a las alertas y el rastreo de la policía en conjunto
-                con las familias afectadas y todas las personas que brindan su ayuda,
-                queremos brindar motivación y aliento para encontrar su ser querido que
-                se encuentra desaparecido.
+                región, esto gracias a las alertas y el rastreo de la policía en
+                conjunto con las familias afectadas y todas las personas que brindan su
+                ayuda, queremos brindar motivación y aliento para encontrar su ser
+                querido que se encuentra desaparecido.
             </p>
 
-        <img src={pilares} alt="pilares" className="pilares" />
-
-
+            <img src={pilares} alt="pilares" className="pilares" />
         </div>
     );
 };
